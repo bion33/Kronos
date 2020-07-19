452453a8e332bea661c6b23cb1842415f57212fc
Krypton-Nova <bion@disroot.org>
2020-07-19 14:39:33 +0200

    Remove v2.2.3

--- bash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Replace the "--help" option below with options of your choice. "--help" or any other invalid option will return the help page.
-python3 Kronos.py --help
-
-# Wait for keypress to exit
-read -n1 -r -p "Press any key to continue..."
--- batch.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-:: Replace the "--help" option below with options of your choice. "--help" or any other invalid option will return the help page.
-python Kronos.py --help
-
-:: Wait for keypress to exit
-PAUSE
